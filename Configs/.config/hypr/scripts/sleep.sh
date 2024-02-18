79050b6561618c7abe3c7a738429e1d433efc571
hdm9527 <1833877078@qq.com>
2024-02-18 21:33:58 +0800

    chore(sleep): remove suspend

--- Configs/.config/hypr/scripts/sleep.sh
+++ Configs/.config/hypr/scripts/sleep.sh
@@ -3,6 +3,6 @@
 swayidle -w timeout 300 'swaylock -f -c 000000' \
             timeout 600 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' \
-            timeout 900 'systemctl suspend' \
+            #timeout 900 'systemctl suspend' \
             before-sleep 'playerctl pause' \
             before-sleep 'swaylock -f -c 000000' &
