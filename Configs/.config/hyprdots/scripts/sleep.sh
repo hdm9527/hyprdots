f306b28a6c63c3ddd48cc9a7eafb2a6ca713a1e6
hdm9527 <1833877078@qq.com>
2024-03-23 17:27:42 +0800

    chore: update idle sleep

--- Configs/.config/hyprdots/scripts/sleep.sh
+++ Configs/.config/hyprdots/scripts/sleep.sh
@@ -3,6 +3,8 @@
 swayidle -w timeout 300 'swaylock -f -c 000000' \
             timeout 600 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' \
+            resume 'pkill wlsunset' \
+            resume 'wlsunset -t 7499 -T 7500' \
             #timeout 900 'systemctl suspend' \
             before-sleep 'playerctl pause' \
             before-sleep 'swaylock -f -c 000000' &
