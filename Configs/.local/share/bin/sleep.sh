a7e3ed73478de7c553a33ee0b9de80e47615a58f
hdm9527 <1833877078@qq.com>
2024-04-13 20:12:43 +0800

    fix(sleep): wlsunset path wrong

--- Configs/.local/share/bin/sleep.sh
+++ Configs/.local/share/bin/sleep.sh
@@ -2,6 +2,6 @@
 
 swayidle -w timeout 300 'swaylock -f -c 000000' \
             timeout 600 'hyprctl dispatch dpms off' \
-            resume '~/.config/hyprdots/scripts/wlsunset.sh' \
+            resume '~/.local/share/bin/wlsunset.sh' \
             timeout 900 'systemctl suspend' \
             before-sleep 'swaylock -f -c 000000'
