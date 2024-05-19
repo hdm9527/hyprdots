cac176fe08561de0e5413158a7ab48ca029c6df9
hdm9527 <1833877078@qq.com>
2024-05-19 20:36:36 +0800

    chore: switch to hypridle

--- Configs/.local/share/bin/sleep.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-swayidle -w timeout 300 'swaylock -f -c 000000' \
-            timeout 600 'hyprctl dispatch dpms off' \
-            resume '~/.local/share/bin/wlsunset.sh' \
-            timeout 900 'systemctl suspend' \
-            before-sleep 'swaylock -f -c 000000'
